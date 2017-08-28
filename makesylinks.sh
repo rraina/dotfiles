2e43c2d478c4880d3081aa68e846352049391142
Rahul Raina <rahulr0609@hotmail.com>
2017-08-28 14:18:04 -0400

    Added gitconfig to repo

--- makesylinks.sh
+++ makesylinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_profile vimrc vim git-completion.bash"    # list of files/folders to symlink in homedir
+files="bash_profile vimrc vim git-completion.bash gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
